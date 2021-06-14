deff144d9b5dec49c19499dfb4760a63efa55df1
Diaz Agustin <carlos.agustin.diaz1998@gmail.com>
2021-06-14 14:01:43 -0300

    change in register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { LoginComponentComponent } from './login-component/login-component.compo
 import { RegisterComponentComponent } from './register-component/register-component.component';
 
 const routes: Routes = [
-  {path: '/login', component: LoginComponentComponent},
-  {path: '/register', component: RegisterComponentComponent},
+  {path: 'login', component: LoginComponentComponent},
+  {path: 'register', component: RegisterComponentComponent},
 ];
 
 @NgModule({
